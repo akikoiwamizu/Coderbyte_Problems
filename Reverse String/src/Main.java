47349efd675b6ce2243e203f3b991b1e7bf58645
Akiko Iwamizu <akikoiwamizu@gmail.com>
2015-09-04 17:59:28 -0400

    Cleaning up

--- Reverse String/src/Main.java
+++ Reverse String/src/Main.java
@@ -1,16 +1,21 @@
-import java.util.*; 
+//Project 1: For this challenge you will be reversing a string.
+//Author: Akiko Iwamizu
+//Language: JAVA
+import java.util.Scanner; 
 
-class Function {  
-  String FirstReverse(String str) { 
+class Function 
+{  
+  private String FirstReverse(String str) 
+  { 
     String reverse = "";
     reverse += new StringBuilder(str).reverse().toString();
     return reverse;
     
   } 
   
-  public static void main (String[] args) {  
-    // keep this function call here     
-	System.out.println("Enter a string:");
+  public static void main (String[] args) 
+  {   
+	System.out.println("Enter a string here:");
     Scanner  s = new Scanner(System.in);
     Function c = new Function();
     System.out.print(c.FirstReverse(s.nextLine())); 
